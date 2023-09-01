96755cf1f9354ae4245a502d19cba2ab100bb1c6
FacundoBisio <facubisio433@gmail.com>
2023-09-01 10:42:20 -0300

    cambios basicos en hmtl

--- peaje/peajeApp/views.py
+++ peaje/peajeApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from peajeApp.models import Usuario
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+    #return HttpResponse('usuarios')
+
+def detalle_usuario(request):
+    return render(request, 'detalle.html')
+
+def usuario(request):
+    usuario = Usuario.objects
