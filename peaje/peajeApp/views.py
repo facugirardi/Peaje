c35f16215028cacdab53160e5880c35c81b792c4
TomiAltero <tomasaltero@gmail.com>
2023-09-04 19:00:11 -0300

    insertar datos panel creacionTurno

--- peaje/peajeApp/views.py
+++ peaje/peajeApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from peajeApp.models import *
 
 # Create your views here.
 def index(request):
@@ -7,6 +8,21 @@ def index(request):
 
 
 def creacionTurno(request):
+    if request.method == 'POST':
+        id_casilla = request.POST.get('idcasilla')
+        id_operador = request.POST.get('idoperador')
+        fh_inicio = request.POST.get('fh-incio')
+        fh_fin = request.POST.get('fh-fin')
+        monto_inicial = request.POST.get('monto-inicial')
+        direccion = request.POST.get('direccion')
+
+        casilla_obj = Casilla.objects.get(pk=id_casilla)
+        operador_obj = Usuario.objects.get(pk=id_operador)
+
+        # Crea el turno con los objetos relacionados
+        turno = TurnoTrabajo(casilla=casilla_obj, usuario=operador_obj, fh_inicio=fh_inicio, fh_fin=fh_fin, monto_inicial=monto_inicial, sentido_cobro=direccion)
+        turno.save()
+
     return render(request, 'turno.html')
 
 
