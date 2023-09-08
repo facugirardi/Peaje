c03a8c50d262329edc6cf07837c9d6f33de8d45f
TomiAltero <tomasaltero@gmail.com>
2023-09-08 09:54:38 -0300

    panel creacion empleado

--- peaje/peajeApp/urls.py
+++ peaje/peajeApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('turno/', views.creacionTurno, name='turno'),
-    path('operador/', views.operador, name='operador')
+    path('operador/', views.operador, name='operador'),
+    path("creacion_empleado/", views.creacion_empleado, name="creacion_empleado")
 ]
--- peaje/peajeApp/views.py
+++ peaje/peajeApp/views.py
@@ -8,23 +8,12 @@ def index(request):
 
 
 def creacionTurno(request):
-    if request.method == 'POST':
-        id_casilla = request.POST.get('idcasilla')
-        id_operador = request.POST.get('idoperador')
-        fh_inicio = request.POST.get('fh-incio')
-        fh_fin = request.POST.get('fh-fin')
-        monto_inicial = request.POST.get('monto-inicial')
-        direccion = request.POST.get('direccion')
-
-        casilla_obj = Casilla.objects.get(pk=id_casilla)
-        operador_obj = Usuario.objects.get(pk=id_operador)
-
-        # Crea el turno con los objetos relacionados
-        turno = TurnoTrabajo(casilla=casilla_obj, usuario=operador_obj, fh_inicio=fh_inicio, fh_fin=fh_fin, monto_inicial=monto_inicial, sentido_cobro=direccion)
-        turno.save()
-
     return render(request, 'turno.html')
 
 
 def operador(request):
     return render(request, 'operador.html')
+
+
+def creacion_empleado(request):
+    return render(request, 'creacion_empleado.html')
