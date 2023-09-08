b90ab55eb9ff02a15621cb336bd09a5b2472aaea
TomiAltero <tomasaltero@gmail.com>
2023-09-08 11:56:01 -0300

    panel creacion empleado

--- peaje/peajeApp/urls.py
+++ peaje/peajeApp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('turno/', views.creacionTurno, name='turno'),
     path('operador/', views.operador, name='operador'),
-    path('login/', views.operador, name='operador')
+    path('login/', views.operador, name='operador'),
+    path('creacion_empleado/', views.creacion_empleado, name='creacion_empleado')
 ]
