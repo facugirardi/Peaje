f49bdc26b6b460dbe559edd2d4fb4f45e93bdd21
TomiAltero <tomasaltero@gmail.com>
2023-09-15 09:49:51 -0300

    insertar datos creacion_empleado

--- peaje/peajeApp/views.py
+++ peaje/peajeApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from .models import *
 
 # Create your views here.
 def base(request):
@@ -18,4 +19,26 @@ def login(request):
     return render(request, 'login.html')
 
 def creacion_empleado(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombre-empleado']
+        apellido = request.POST['apellido-empleado']
+        direccion = request.POST['direccion-empleado']
+        correo = request.POST['email-empleado']
+        tipo_documento = request.POST['tipo-documento-empleado']
+        nro_documento = request.POST['nro-documento-empleado']
+        pass_empleado = request.POST['pass-empleado']
+
+
+        empleado = Usuario(nombre=nombre, 
+                           apellido=apellido, 
+                           direccion=direccion,
+                           email=correo, 
+                           tipo_documento=tipo_documento,
+                           numero_documento=nro_documento,
+                           password=pass_empleado,
+                           permisos= True
+                           )
+        
+        empleado.save()
+
     return render(request, 'creacion_empleado.html')
