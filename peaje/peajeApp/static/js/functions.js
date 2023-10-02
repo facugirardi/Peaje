194d1534d4cd6c3470240a41dbb723fc5c9f37b8
TomiAltero <tomasaltero@gmail.com>
2023-10-02 17:58:04 -0300

    eliminacion archivo JS innecesario

--- peaje/peajeApp/static/js/functions.js
+++
@@ -1,7 +0,0 @@
-//Funcion para ocultar el mensaje de error o exito ingresado por el usuario
-setTimeout(function() {
-    var messages = document.querySelectorAll('.messages li');
-    messages.forEach(function(message) {
-        message.style.display = 'none';
-    });
-}, 1500);
