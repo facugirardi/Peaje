409520b02038814f017cc146019d318700ad8663
facugirardi <facugirardi22@gmail.com>
2023-10-05 12:12:23 -0300

    login a la mitad

--- peaje/peajeApp/urls.py
+++ peaje/peajeApp/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('turno/', views.CreacionTurnoView.as_view(), name='turno'),
     path('operador/', views.operador, name='operador'),
     path('creacion_empleado/', views.CreacionEmpleadoView.as_view(), name='creacion_empleado'),
-    path('login/', views.login, name='login')
+    path('login/', views.LoginView.as_view(), name='login')
 ]
