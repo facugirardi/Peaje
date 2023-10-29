f57e1748ce8ed72e3569f531f93ce6f76029dcd4
facugirardi <facugirardi22@gmail.com>
2023-10-29 19:25:23 -0300

    seleccionar elemtos operador

--- peaje/peajeApp/static/js/main.js
+++ peaje/peajeApp/static/js/main.js
@@ -1,21 +1,39 @@
+
 const inputIdEstacion = document.getElementById("input-id-estacion");
-const selectCasilla = document.getElementById("input-id-casilla");
-const casillaOptions = [...selectCasilla];
-console.log(casillaOptions);
 
-inputIdEstacion.addEventListener("change", function () {
-    const selectedIdEstacion = inputIdEstacion.value;
+if(inputIdEstacion) {
 
-    casillaOptions.forEach(option => {
-        option.style.display = "";
+    const selectCasilla = document.getElementById("input-id-casilla");
+    const casillaOptions = [...selectCasilla];
+    
+
+    inputIdEstacion.addEventListener("change", function () {
+        const selectedIdEstacion = inputIdEstacion.value;
+
+        casillaOptions.forEach(option => {
+            option.style.display = "";
+        });
+
+        if (selectedIdEstacion) {
+            casillaOptions.forEach(option => {
+                const estacionId = option.getAttribute("data-estacion");
+                if (estacionId !== selectedIdEstacion) {
+                    option.style.display = "none";
+                }
+            });
+        }
     });
 
-    if (selectedIdEstacion) {
-        casillaOptions.forEach(option => {
-            const estacionId = option.getAttribute("data-estacion");
-            if (estacionId !== selectedIdEstacion) {
-                option.style.display = "none";
-            }
-        });
-    }
+
+}
+
+$(document).ready(function() {
+    $('.card').click(function() {
+        if ($(this).hasClass('selected')) {
+            $(this).removeClass('selected');
+        } else {
+            $('.card').removeClass('selected'); 
+            $(this).addClass('selected'); 
+        }
+    });
 });
