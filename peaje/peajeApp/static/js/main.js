721a84a9baad8881adc30a996a44b26a6997ca7a
facugirardi <facugirardi22@gmail.com>
2023-10-29 22:22:47 -0300

    generacion de ticket

--- peaje/peajeApp/static/js/main.js
+++ peaje/peajeApp/static/js/main.js
@@ -28,7 +28,7 @@ if(inputIdEstacion) {
 }
 
 $(document).ready(function() {
-    $('.card').click(function() {
+    $('.card-select').click(function() {
         if ($(this).hasClass('selected')) {
             $(this).removeClass('selected');
         } else {
@@ -37,3 +37,43 @@ $(document).ready(function() {
         }
     });
 });
+
+    $('.emitir').on('click', function() {
+        var selectedId = $('.selected').attr('id');
+
+        if(selectedId){
+
+            var resultadoDiv = '';
+        
+            switch (selectedId) {
+                case 'elemento1':
+                    resultadoDiv = '1; Motocicleta; 40';
+                    break;
+                case 'elemento2':
+                    resultadoDiv = '2; Automoviles; 80';
+                    break;
+                case 'elemento3':
+                    resultadoDiv = '3; 2 Ejes Con Ruedas Duales o Altura Mayor a 2,10m; 160';
+                    break;
+                case 'elemento4':
+                    resultadoDiv = '4; 3 o 4 Ejes Sin Ruedas Duales y Altura Menor a 2,10m; 160';
+                    break;
+                case 'elemento5':
+                    resultadoDiv = '5; 3 o 4 Ejes Con Ruedas Duales o Altura Mayor a 2,10m; 240';
+                    break;
+                case 'elemento6':
+                    resultadoDiv = '6; 5 o 6 Ejes; 320';
+                    break;
+                case 'elemento7':
+                    resultadoDiv = '7; Más de 6 Ejes; 400';
+                    break;
+            }    
+
+            console.log(resultadoDiv);
+
+            $('.selected').removeClass('selected');
+
+        }
+
+    });
+
